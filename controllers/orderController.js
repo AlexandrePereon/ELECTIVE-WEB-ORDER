a6958ed5dd3196123128e421e809f6f6f52d788d
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-03-28 15:59:24 +0100

    API documentation and accept order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -12,7 +12,7 @@ const orderController = {
 
     // Check if the menus or articles are not empty
     if (!menus.length && !articles.length) {
-      return res.status(400).send('No menus or articles provided');
+      return res.status(400).send('Menus ou articles non fournis');
     }
 
     try {
@@ -21,7 +21,7 @@ const orderController = {
 
       // Check if the restaurant exists
       if (!restaurant) {
-        return res.status(404).send('Restaurant not found');
+        return res.status(404).send('Restaurant non trouvé');
       }
 
       // Get the menus and articles by their IDs and the restaurant
@@ -34,7 +34,7 @@ const orderController = {
 
       // Check if all products are from the same restaurant
       if (menusRestaurant.length !== new Set(menus).size || articlesRestaurant.length !== new Set(articles).size) {
-        return res.status(400).send('All products must be from the same restaurant');
+        return res.status(400).send('Tous les produits ne sont pas du même restaurant');
       }
 
       const transformedMenus = menusRestaurant.map((menu) => ({
@@ -68,13 +68,53 @@ const orderController = {
       OrderSub.publish('marketingUpdated');
       OrderSub.publish(`restaurantUpdated-${restaurantId}`, restaurantId);
 
-      return res.status(200).json({ order: neworder, message: 'Commande créée avec succès' });
+      return res.status(200).json({ order: neworder._id, message: 'Commande créée avec succès' });
     } catch (error) {
       console.error(error);
       return res.status(500).send('Internal server error');
     }
   },
   // PUT /order/accept
+  accept: async (req, res) => {
+    // The restaurant accepts the order
+    const { orderId } = req.body;
+    const restaurantId = req.restaurant._id;
+
+    console.log(req.body);
+
+    try {
+      // Find the order
+      const order = await Order.findById(orderId);
+
+      // Check if the order exists
+      if (!order) {
+        return res.status(404).send('Commande non trouvée');
+      }
+
+      // Check if the order is from the same restaurant
+      if (order.restaurant_id.toString() !== restaurantId.toString()) {
+        return res.status(400).send('Cette commande n\'appartient pas à ce restaurant');
+      }
+
+      // Check if the order is not already accepted
+      if (order.status === 'En préparation') {
+        return res.status(400).send('La commande est déjà en cours de préparation');
+      }
+
+      // Update the order status
+      order.status = 'En préparation';
+      await order.save();
+
+      // Notify via websocket
+      OrderSub.publish('marketingUpdated');
+      OrderSub.publish(`restaurantUpdated-${restaurantId}`, restaurantId);
+
+      return res.status(200).send('Commande acceptée avec succès');
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send('Internal server error');
+    }
+  },
 };
 
 const sendMarketingData = (ws) => async (restaurantId) => {
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import authMiddleware from '../middlewares/authMiddleware.js';
 import isUserMiddleware from '../middlewares/isUserMiddleware.js';
+import isRestaurantMiddleware from '../middlewares/isRestaurantMiddleware.js';
+import hasRestaurantMiddleware from '../middlewares/hasRestaurantMiddleware.js';
 import { orderController } from '../controllers/orderController.js';
 
 const orderRouter = express.Router();
@@ -9,9 +11,11 @@ const orderRouter = express.Router();
  * @swagger
  * /order/create/:
  *   post:
- *     summary: Get an article by ID
- *     description: This endpoint retrieves an article by its unique identifier.
+ *     summary: Create a new order
+ *     description: This endpoint allows the creation of a new order including specified menus and articles within a particular restaurant. It ensures that menus and articles are provided, validates the existence of the restaurant, checks if all items belong to the same restaurant, calculates the total price, and finally, creates and saves the new order.
  *     tags: [Order]
+ *     security:
+ *       - BearerAuth: []
  *     requestBody:
  *       required: true
  *       content:
@@ -19,25 +23,43 @@ const orderRouter = express.Router();
  *           schema:
  *             type: object
  *             required:
- *               - name
+ *               - menus
+ *               - articles
+ *               - restaurantId
  *             properties:
  *               menus:
- *                 type: string
- *                 example: ['6602bc8fdbbe228cf99bc07f','6602bc8fdbbe228cf99bc07f']
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: An array of menu IDs to include in the order.
+ *                 example: ['6602bc8fdbbe228cf99bc07f', '6602bc8fdbbe228cf99bc07f']
  *               articles:
- *                 type: string
- *                 example: ['6602bc5bdbbe228cf99bc077','6602bc75dbbe228cf99bc07b']
+ *                 type: array
+ *                 items:
+ *                   type: string
+ *                 description: An array of article IDs to include in the order.
+ *                 example: ['6602bc5bdbbe228cf99bc077', '6602bc75dbbe228cf99bc07b']
  *               restaurantId:
  *                 type: string
+ *                 description: The unique identifier of the restaurant for the order.
  *                 example: '6602bc45dbbe228cf99bc073'
  *     responses:
  *       200:
- *         description: Successfully retrieved the article
+ *         description: Order successfully created
  *         content:
  *           application/json:
  *             schema:
- *       404:
- *         description: Article not found
+ *               type: object
+ *               properties:
+ *                 order:
+ *                   type: string
+ *                   description: The unique identifier of the created order.
+ *                   example: '6612bc45dbbe228cf99bc078'
+ *                 message:
+ *                   type: string
+ *                   example: 'Commande créée avec succès'
+ *       400:
+ *         description: Validation error such as no menus or articles provided, or items not from the same restaurant
  *         content:
  *           application/json:
  *             schema:
@@ -46,10 +68,99 @@ const orderRouter = express.Router();
  *                 message:
  *                   type: string
  *                   description: Detailed error message.
- *                   example: 'Article not found'
- *     security:
- *       - BearerAuth: []
+ *                   example: 'Menus ou articles non fournis'
+ *       404:
+ *         description: Restaurant not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Detailed error message.
+ *                   example: 'Restaurant non trouvé'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Detailed error message.
+ *                   example: 'Internal server error'
  */
 orderRouter.post('/create', authMiddleware, isUserMiddleware, orderController.create);
+
+/**
+ * @swagger
+ * /order/accept/:
+ *   post:
+ *     summary: Accept an order
+ *     description: This endpoint allows a restaurant to accept an order by its ID. It verifies that the order exists, checks if the order belongs to the restaurant making the request, and ensures the order has not been previously accepted before updating its status.
+ *     tags: [Order]
+ *     security:
+ *       - BearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - orderId
+ *             properties:
+ *               orderId:
+ *                 type: string
+ *                 description: The unique identifier of the order to be accepted.
+ *                 example: '6612bc45dbbe228cf99bc078'
+ *     responses:
+ *       200:
+ *         description: Order successfully accepted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: 'Commande acceptée avec succès'
+ *       400:
+ *         description: Validation errors such as order not from this restaurant or order already accepted
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Detailed error message.
+ *                   example: 'La commande n est pas du même restaurant'
+ *       404:
+ *         description: Order not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Detailed error message.
+ *                   example: 'Commande non trouvée'
+ *       500:
+ *         description: Internal server error
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   description: Detailed error message.
+ *                   example: 'Internal server error'
+ */
+orderRouter.post('/accept', authMiddleware, isRestaurantMiddleware, hasRestaurantMiddleware, orderController.accept);
 
 export default orderRouter;
