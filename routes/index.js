33e474aac95ae6017efd0687bf0b8a88732f669f
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-03-28 10:10:11 +0100

    fix router

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const router = express.Router();
 
 fs.readdirSync(__dirname).forEach(async (file) => {
-  if (file in ['index.js', 'prometheusRoutes.js']) return;
+  if (file === 'index.js' || file === 'prometheus.js') return;
   const route = await import(`./${file}`);
   router.use(route.default);
 });
