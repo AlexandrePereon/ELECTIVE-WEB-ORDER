d73911bf2fa102f5f0355dcdd5d6b4fa7bd721ba
AlexandrePereon <alexandre.pereon.pro@gmail.com>
2024-04-02 08:55:39 +0200

    No message on init socket

--- controllers/marketingController.js
+++ controllers/marketingController.js
@@ -2,13 +2,7 @@ import OrderSub from '../utils/orderSubscription.js';
 import { sendMarketingData } from './orderController.js';
 
 const marketingSocketController = async (ws) => {
-  ws.send('Connected to the marketing socket');
-
   sendMarketingData(ws)();
-
-  ws.on('message', (msg) => {
-    ws.send(`Message received: ${msg}`);
-  });
 
   OrderSub.subscribe('marketingUpdated', sendMarketingData(ws));
 
--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -2,14 +2,9 @@ import OrderSub from '../utils/orderSubscription.js';
 import { sendMarketingData } from './orderController.js';
 
 const restaurantSocketController = async (ws, req) => {
-  ws.send('Connected to the restaurant socket');
   const { restaurant } = req;
 
   sendMarketingData(ws)(restaurant._id);
-
-  ws.on('message', (msg) => {
-    ws.send(`Message received: ${msg}`);
-  });
 
   OrderSub.subscribe(`restaurantUpdated-${restaurant._id}`, sendMarketingData(ws));
 
